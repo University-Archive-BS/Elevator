a01898c0d10202fbfdbca2bde823701cb35e6123
Ali Nazari <ali.nazari@aut.ac.ir>
2018-12-15 22:22:12 +0330

    add every thing to the main module

--- aha_final/Main.v
+++ aha_final/Main.v
@@ -20,6 +20,24 @@
 //////////////////////////////////////////////////////////////////////////////////
 module Main();
 	
-
+	input start_star;
+	input [3:0] hash; //4'b1011
+	
+	input reg [3:0] username_1;
+	input reg [3:0] username_10;
+	input reg [3:0] username_100;
+	
+	input reg [3:0] password_1;
+	input reg [3:0] password_10;
+	input reg [3:0] password_100;
+	input reg [3:0] password_1000;
+	
+	parameter [5:0] S0 = 0, S1 = 1, S2 = 2, S3 = 3;
+	
+	always @ (*)
+		if (start_star) 
+			begin
+				
+			end
 
 endmodule
