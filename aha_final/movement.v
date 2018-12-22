5a095a3d66893811634ba48d7f862cd5110af129
Ali Nazari <ali.nazari@aut.ac.ir>
2018-12-22 17:13:58 +0330

    add movement module

---
+++ aha_final/movement.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:13:33 12/22/2018 
+// Design Name: 
+// Module Name:    movement 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module movement();
+
+	
+
+
+endmodule
