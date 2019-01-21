68123f3099bc3f376f0b4e3745945dcf928d084e
Ali Nazari <ali.nazari@aut.ac.ir>
2019-01-21 18:34:35 +0330

    instantiate movement and management in elevator and login as a simple user in the testbench but the logged_in signal doesn't work correctly

--- aha_final/elevator.v
+++ aha_final/elevator.v
@@ -7,8 +7,18 @@
 // Project Name: Smart-Elevator 
 // Revision 0.01 - File Created
 //////////////////////////////////////////////////////////////////////////////////
-module elevator(
-    );
+module elevator(CLK, RST, BCD_management, engine, doors, interior_movement, exterior_movement);
 
+	input RST, CLK;
+	input [3:0] BCD_management;
+	input [2:0] interior_movement, exterior_movement;
+	
+	output [1:0] engine;
+	output [2:0] doors;
+	
+	reg logged_in = 0;
+	
+	management manage(CLK, RST, BCD_management, logged_in);
+	movement move(engine, doors, CLK, RST, interior_movement, exterior_movement, logged_in);
 
 endmodule
